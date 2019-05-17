47781110ead8dc0d3f07c35257d921e2d789f259
Robert Cereghini <rcereghini@Roberts-MacBook-Pro.local>
2019-05-16 22:30:42 -0700

    Logo text change

--- src/App.js
+++ src/App.js
@@ -6,17 +6,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          style="border-radius: 50%"
+        />
+        <p>COMING SOON: Robert Cereghini</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="www.rcereghini.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View Old Portfolio
         </a>
       </header>
     </div>
