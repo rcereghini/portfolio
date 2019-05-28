e2bd5080252b0a92744f0712eb89c86386a286fe
rcereghini <robertc@petroleumrx.com>
2019-05-28 10:37:11 -0700

    removed spin class

--- src/App.js
+++ src/App.js
@@ -3,26 +3,17 @@ import logo from "./bob.jpeg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-          style={{ borderRadius: "50%" }}
-        />
-        <p>COMING SOON: Robert Cereghini</p>
-        <a
-          className="App-link"
-          href="https://rcereghini.github.io/"
-          target="_blank"
-        >
-          Old Portfolio
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} alt="logo" style={{ borderRadius: "50%" }} />
+				<p>COMING SOON: Robert Cereghini</p>
+				<a className="App-link" href="https://rcereghini.github.io/" target="_blank">
+					Old Portfolio
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
